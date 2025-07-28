af8fdf0798665c97ba585c7a0357b00a2890c72d
Iman Enami <iman.enami@canonical.com>
2025-07-28 10:07:58 +0400

    [DPE-7765] feat: base charm with TLS and ingress support

---
+++ src/charm.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical
+# See LICENSE file for licensing details.
+
+"""Charm the application."""
+
+import logging
+
+import ops
+import requests
+from charms.data_platform_libs.v0.data_interfaces import (
+    KafkaConnectRequirerEventHandlers,
+    KafkaRequirerEventHandlers,
+)
+from charms.data_platform_libs.v0.data_models import TypedCharmBase
+from charms.data_platform_libs.v0.karapace import KarapaceRequiresEventHandlers
+from charms.traefik_k8s.v2.ingress import IngressPerAppRequirer
+from ops import CollectStatusEvent
+from tenacity import retry, retry_if_exception, stop_after_attempt, wait_fixed
+
+from core.models import Context
+from core.structured_config import CharmConfig
+from events.tls import TLSHandler
+from literals import (
+    CONTAINER,
+    KAFKA_CONNECT_REL,
+    KAFKA_REL,
+    KARAPACE_REL,
+    SUBSTRATE,
+    DebugLevel,
+    Status,
+)
+from managers.config import ConfigManager
+from managers.tls import TLSManager
+from workload import Workload
+
+logger = logging.getLogger(__name__)
+
+
+class KafkaUiCharm(TypedCharmBase[CharmConfig]):
+    """Charm the application."""
+
+    config_type = CharmConfig
+
+    def __init__(self, framework: ops.Framework):
+        super().__init__(framework)
+
+        self.workload = Workload(container=self.unit.get_container(CONTAINER))
+        self.context = Context(self)
+        self.pending_inactive_statuses: list[Status] = []
+
+        # Managers
+        self.config_manager = ConfigManager(
+            context=self.context, workload=self.workload, config=self.config
+        )
+        self.tls_manager = TLSManager(
+            context=self.context, workload=self.workload, substrate=SUBSTRATE
+        )
+
+        # Handlers
+        self.kafka_events = KafkaRequirerEventHandlers(self, self.context.kafka_client_interface)
+        self.connect_events = KafkaConnectRequirerEventHandlers(
+            self, self.context.connect_client_interface
+        )
+        self.karapace_events = KarapaceRequiresEventHandlers(
+            self, self.context.karapace_client_interface
+        )
+        self.tls = TLSHandler(self)
+
+        self.framework.observe(self.on.upgrade_charm, self._on_upgrade_charm)
+        self.framework.observe(self.on.config_changed, self._on_config_changed)
+        self.framework.observe(self.on.update_status, self._on_update_status)
+        self.framework.observe(self.on.collect_unit_status, self._on_collect_status)
+        self.framework.observe(self.on.collect_app_status, self._on_collect_status)
+
+        for relation in [KAFKA_REL, KAFKA_CONNECT_REL, KARAPACE_REL]:
+            self.framework.observe(self.on[relation].relation_changed, self._on_config_changed)
+            self.framework.observe(self.on[relation].relation_broken, self._on_config_changed)
+
+        if SUBSTRATE == "k8s":
+            self.ingress = IngressPerAppRequirer(self, port=8080, scheme="http")
+
+    def _on_config_changed(self, event: ops.EventBase) -> None:
+        """Handle `config-changed` and general client `relation-changed` events."""
+        if not all([self.workload.container_can_connect, self.context.app]):
+            event.defer()
+            return
+
+        self.tls.init_unit_tls()
+
+        if not self.context.app.admin_password:
+            self.context.app.update(
+                {self.context.app.ADMIN_PASSWORD: self.workload.generate_password()}
+            )
+
+        config_changed = self.config_manager.config_changed()
+        truststore_changed = self.tls_manager.truststore_changed()
+
+        if not any([config_changed, truststore_changed]):
+            return
+
+        if truststore_changed:
+            self.tls_manager.update_truststore()
+
+        self.workload.set_environment(env_vars=self.config_manager.java_opts)
+        self.workload.write(
+            content=self.config_manager.clean_yaml_config,
+            path=self.workload.paths.application_local_config,
+        )
+
+        self.workload.restart()
+
+    def _on_update_status(self, _) -> None:
+        """Handle `update-status` event."""
+        logger.debug("Update status, emitting config-changed")
+        self.on.config_changed.emit()
+
+    def _on_upgrade_charm(self, _: ops.EventBase) -> None:
+        """Handle `upgrade-charm` event."""
+        self.on.config_changed.emit()
+
+    def _set_status(self, key: Status) -> None:
+        """Set charm status."""
+        status: ops.StatusBase = key.value.status
+        log_level: DebugLevel = key.value.log_level
+
+        getattr(logger, log_level.lower())(status.message)
+        self.pending_inactive_statuses.append(key)
+
+    def _on_collect_status(self, event: CollectStatusEvent):
+        """Handle `collect-status` event."""
+        self.health_check()
+        for status in self.pending_inactive_statuses + [Status.ACTIVE]:
+            event.add_status(status.value.status)
+
+    @retry(
+        wait=wait_fixed(1),
+        stop=stop_after_attempt(3),
+        retry=retry_if_exception(lambda _: True),
+        retry_error_callback=lambda _: False,
+    )
+    def health_check(self) -> bool:
+        """Check if workload and web server are healthy and up."""
+        if not all([self.workload.container_can_connect, self.workload.installed]):
+            self._set_status(Status.INSTALLING)
+            return False
+
+        if not self.context.kafka_client.ready:
+            self._set_status(Status.MISSING_KAFKA)
+            return False
+
+        if SUBSTRATE == "k8s" and not self.ingress.is_ready():
+            self._set_status(Status.MISSING_INGRESS)
+            return False
+
+        if not self.workload.active():
+            self._set_status(Status.SERVICE_NOT_RUNNING)
+            return False
+
+        resp = requests.get(self.context.endpoint, verify=False, timeout=2)
+
+        if not resp.status_code == 200:
+            self._set_status(Status.SERVICE_UNHEALTHY)
+            return False
+
+        return True
+
+
+if __name__ == "__main__":  # pragma: nocover
+    ops.main(KafkaUiCharm)
---
+++ src/workload.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Kafka Connect workload class and methods."""
+
+import logging
+import socket
+from contextlib import closing
+from typing import Iterable, Mapping
+
+from charmlibs import pathops
+from ops import Container, pebble
+from tenacity import retry, retry_if_result, stop_after_attempt, wait_fixed
+from typing_extensions import override
+
+from core.workload import WorkloadBase
+from literals import GROUP, SERVICE_NAME, USER_NAME
+
+logger = logging.getLogger(__name__)
+
+
+class Workload(WorkloadBase):
+    """Wrapper for performing common operations specific to the Kafka Connect Snap."""
+
+    service: str = SERVICE_NAME
+    SNAP_NAME = "charmed-kafka-ui"
+
+    def __init__(self, container: Container, profile: str = "production") -> None:
+        self.container = container
+        self.root = pathops.ContainerPath("/", container=container)
+
+    @override
+    def start(self) -> None:
+        self.container.add_layer("kafka-ui", self.layer, combine=True)
+        self.container.restart(self.service)
+
+    @override
+    def stop(self) -> None:
+        self.container.stop(self.service)
+
+    @override
+    def restart(self) -> None:
+        self.start()
+
+    @override
+    def read(self, path: str) -> list[str]:
+        return (
+            [] if not (self.root / path).exists() else (self.root / path).read_text().split("\n")
+        )
+
+    @override
+    def write(self, content: str, path: str, mode: str = "w") -> None:
+        (self.root / path).write_text(content, user=USER_NAME, group=GROUP)
+
+    @override
+    def exec(
+        self,
+        command: list[str] | str,
+        env: Mapping[str, str] | None = None,
+        working_dir: str | None = None,
+    ) -> str:
+        command = command if isinstance(command, list) else [command]
+        try:
+            process = self.container.exec(
+                command=command,
+                environment=env,
+                working_dir=working_dir,
+                combine_stderr=True,
+            )
+            output, _ = process.wait_output()
+            return output
+        except pebble.ExecError as e:
+            logger.error(f"cmd failed - cmd={command}, stdout={e.stdout}, stderr={e.stderr}")
+            logger.error(e)
+            raise e
+
+    @override
+    @retry(
+        wait=wait_fixed(1),
+        stop=stop_after_attempt(5),
+        retry=retry_if_result(lambda result: result is False),
+        retry_error_callback=lambda _: False,
+    )
+    def active(self) -> bool:
+        if not self.installed:
+            return False
+
+        if self.service not in self.container.get_services():
+            return False
+
+        return self.container.get_service(self.service).is_running()
+
+    @override
+    def check_socket(self, host: str, port: int) -> bool:
+        with closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as sock:
+            return sock.connect_ex((host, port)) == 0
+
+    @property
+    @override
+    def installed(self) -> bool:
+        """Whether the workload service is installed or not."""
+        if not self.container.can_connect():
+            return False
+
+        return True
+
+    @property
+    @override
+    def container_can_connect(self) -> bool:
+        return self.container.can_connect()
+
+    @property
+    @override
+    def layer(self) -> pebble.Layer:
+        command = [
+            "java",
+            f"-Dspring.config.additional-location={self.paths.config_dir}/application-local.yml",
+            "--add-opens",
+            "java.rmi/javax.rmi.ssl=ALL-UNNAMED",
+            "-Xms1G",
+            "-Xmx1G",
+            "-XX:+UseG1GC",
+            "-jar",
+            "/opt/kafka-ui/libs/api-1.2.0.jar",
+        ]
+
+        layer_config: pebble.LayerDict = {
+            "summary": "Kafka UI Layer",
+            "description": "Pebble config layer for Apache Kafka UI",
+            "services": {
+                self.service: {
+                    "override": "merge",
+                    "summary": "Kafka UI",
+                    "command": " ".join(command),
+                    "startup": "enabled",
+                    "user": USER_NAME,
+                    "group": GROUP,
+                }
+            },
+        }
+        return pebble.Layer(layer_config)
+        raise NotImplementedError
+
+    @override
+    def set_environment(self, env_vars: Iterable[str]) -> None:
+        raw_current_env = self.read(self.paths.env)
+        current_env = self.map_env(raw_current_env)
+
+        updated_env = current_env | self.map_env(env_vars)
+        content = "\n".join([f"{key}={value}" for key, value in updated_env.items()])
+        self.write(content=content + "\n", path=self.paths.env)
+
+    @staticmethod
+    def map_env(env: Iterable[str]) -> dict[str, str]:
+        """Parse env variables into a dict."""
+        map_env = {}
+        for var in env:
+            key = "".join(var.split("=", maxsplit=1)[0])
+            value = "".join(var.split("=", maxsplit=1)[1:])
+            if key:
+                # only check for keys, as we can have an empty value for a variable
+                map_env[key] = value
+        return map_env
